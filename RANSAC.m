ae50ade775afa035f0d6f9fb13c43c4c2a8c98c6
Michael Brunetti <mike.brunetti15@gmail.com>
2016-03-15 10:05:14 -0400

    changes to ransac, added ignore file

--- RANSAC.m
+++ RANSAC.m
@@ -28,16 +28,41 @@ while (1==1)
     [U, ~, ~] = svd(A.'*A);
     homography = U(:,size(U,2));
     
+    h = zeros(3,3);
     h(1,:)=homography(1:3);
     h(2,:)=homography(4:6);
     h(3,:)=homography(7:9);
     
-    for i=1:num_matches
+    points_1_proj = ones(num_matches, 3);
+    points_1_proj(:,1:2)=points_1;
+    points_1_prime_proj = zeros(num_matches,3);
     
+    inliers = 0;
+    inliers_1_buff = zeros(num_matches,2);
+    inliers_2_buff = zeros(num_matches,2);
+    
+    for i=1:num_matches
+        points_1_prime_proj(i,:) = h*points_1_proj(i,:);
+        points_1_prime = points_1_prime_proj(i,1:2);
 
+        dist = norm(points_1_prime - points_2(i,:));
+        
+        if ( dist < dist_thresh )
+            inliers = inliers+1;
+            inliers_1_buff(inliers,:)= points_1(i,:);
+            inliers_2_buff(inliers,:)= points_2(i,:);
+        end
+    end
+    
+    pct_inliers = inliers / num_matches;
+    inliers_1 = inliers_1_buff(1:inliers,:);
+    inliers_2 = inliers_2_buff(1:inliers,:);
+    
+    if ( pct_inliers > inliers_thresh )
+        break;
+    end
 
 end
 
 
 end
-
