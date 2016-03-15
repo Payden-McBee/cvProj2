07dccb2c8ebc1e7c03b8c08a388e73a079a38041
Michael Brunetti <mike.brunetti15@gmail.com>
2016-03-15 16:18:36 -0400

    fixed calculation of homography matrix

--- homogFromSVD.m
+++ homogFromSVD.m
@@ -2,27 +2,27 @@ function homog = homogFromSVD(inliers1,inliers2)
 numPts = size(inliers1,1);
 A = zeros(2*numPts,9);
 for i=1:numPts
-    %number is even
-    A(2*i-1,1)=0;
-    A(2*i-1,2)=0;
-    A(2*i-1,3)=0;
-    A(2*i-1,4)=inliers1(i,1);
-    A(2*i-1,5)=inliers1(i,2);
-    A(2*i-1,6)=1;
-    A(2*i-1,7)=(-1)*inliers1(i,1)*inliers2(i,2);
-    A(2*i-1,8)=(-1)*inliers1(i,2)*inliers2(i,2);
-    A(2*i-1,9)=(-1)*inliers2(i,2);
-
     %number is odd
-    A(2*i,1)=inliers1(i,1);
-    A(2*i,2)=inliers1(i,2);
-    A(2*i,3)=1;
-    A(2*i,4)=0;
-    A(2*i,5)=0;
-    A(2*i,6)=0;
-    A(2*i,7)=(-1)*inliers1(i,1)*inliers2(i,1);
-    A(2*i,8)=(-1)*inliers1(i,2)*inliers2(i,1);
-    A(2*i,9)=(-1)*inliers2(i,1);
+    A(2*i-1,1)=inliers1(i,1);
+    A(2*i-1,2)=inliers1(i,2);
+    A(2*i-1,3)=1;
+    A(2*i-1,4)=0;
+    A(2*i-1,5)=0;
+    A(2*i-1,6)=0;
+    A(2*i-1,7)=(-1)*inliers1(i,1)*inliers2(i,1);
+    A(2*i-1,8)=(-1)*inliers1(i,2)*inliers2(i,1);
+    A(2*i-1,9)=(-1)*inliers2(i,1);
+    
+    %number is even
+    A(2*i,1)=0;
+    A(2*i,2)=0;
+    A(2*i,3)=0;
+    A(2*i,4)=inliers1(i,1);
+    A(2*i,5)=inliers1(i,2);
+    A(2*i,6)=1;
+    A(2*i,7)=(-1)*inliers1(i,1)*inliers2(i,2);
+    A(2*i,8)=(-1)*inliers1(i,2)*inliers2(i,2);
+    A(2*i,9)=(-1)*inliers2(i,2);
 end
 
 [U,S,V] = svd(A);
@@ -31,6 +31,3 @@ H = V(:,lastCol);
 homog = [H(1) H(2) H(3);
          H(4) H(5) H(6);
          H(7) H(8) H(9)];
-    
-    
-    
