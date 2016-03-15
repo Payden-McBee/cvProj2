1c3b8d64555e1d114e4f94dd5523f38bf5ad75f0
Michael Brunetti <mike.brunetti15@gmail.com>
2016-03-15 12:00:30 -0400

    minor changes

--- cvP2.m
+++ cvP2.m
@@ -38,6 +38,6 @@ dist_thresh = 10;
 inliers_thresh = 0.9;
 [inliers_1, inliers_2] = RANSAC( corners_1, corners_2, matches, dist_thresh, inliers_thresh);
 
-
+h = homogFromSVD( inliers_1, inliers_2 );
 
 %% Warp one image onto the other 
