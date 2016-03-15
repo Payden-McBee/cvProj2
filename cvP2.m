37081ea9cd3813a734e0640d498a8c8bad37ccfd
Michael Brunetti <mike.brunetti15@gmail.com>
2016-03-14 20:21:53 -0400

    changes to main file

--- cvP2.m
+++ cvP2.m
@@ -10,15 +10,23 @@ img2 = imgs(:,:,2);
 
 %% Apply Harris Corner Detector to images
 % Defining the image region which contains the corners
-R_surf_1 = harris_corners ( img1, 5 );
-R_surf_2 = harris_corners ( img2, 5 );
+harris_win_size = 5;
+R_surf_1 = harris_corners ( img1, harris_win_size );
+R_surf_2 = harris_corners ( img2, harris_win_size );
 
 %% Non-Maximum Supression to find sparse set of corner features
+%placeholders
+corners_1 = non_max( R_surf_1 );
+corners_2 = non_max( R_surf_2 );
 
-%% Compute NCC (normalized cross correlation) 
 
-%% Choose pair of corners with highest NCC value
+%% Compute NCC (normalized cross correlation), threshold
+NCC_win_size = 5;
+NCC_mat = calc_NCC( img1, corners_1, img2, corners_2, NCC_win_size );
 
 %% Estimate the homography using chosen corners using RANSAC
+
+
+
 
 %% Warp one image onto the other 
